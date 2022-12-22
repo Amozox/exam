7703e0117cb993a57c8fdc96a81fd87789540cdc
Amozox <32430013+Amozox@users.noreply.github.com>
2022-12-22 15:18:40 +0100

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -1,119 +1,85 @@
-#include <stdlib.h>
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_slit.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rperrin <rperrin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/22 00:42:35 by rperrin           #+#    #+#             */
+/*   Updated: 2021/10/22 01:31:27 by rperrin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	ft_get_len(char *str, int i, char c)
+#include "libft.h"
+
+static int	normecmort(int *i, char const *s, char c)
 {
-	int	len;
+	int	j;
 
-	len = 0;
-	while (str[i] != c && str[i++])
-		len++;
-	return (len);
-}
-
-int	ft_count_word(char *str, char c, int i)
-{
-	int	count;
-
-	count = 0;
-	while (str[i])
-	{
-		if (str[i] != c)
-		{
-			while (str[i] != c || str[i] == '\0')
-				i++;
-			count++;
-		}
-		i++;
-	}
-	return (count);
-}
-
-int	test(char *str, int i, char c)
-{
-	while (str[i])
-	{
-		if (str[i] == c)
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-char	*ft_strdup(char *src)
-{
-	int		i;
-	int		len;
-	char	*str;
-
-	len = 0;
-	while (src[len])
-		len++;
-	str = (char*)malloc(sizeof(*str) * (len + 1));
-	i = 0;
-	while (i < len)
-	{
-		str[i] = src[i];
-		i++;
-	}
-	return (str);
-}
-
-char	**ft_split(char *str, char c)
-{
-	char	**res;
-	int		i;
-	int		j;
-	int		x;
-
-	i = 0;
+	while (s[*i] && s[*i] == c)
+		(*i)++;
 	j = 0;
-	x = 0;
-	if (test(str, i, c))
+	while (s[*i + j] && s[*i + j] != c)
+		j++;
+	return (j);
+}
+
+static char	**error(char **res, int n)
+{
+	while (n--)
+		free(res[n]);
+	free(res);
+	return (NULL);
+}
+
+static char	**malloc_res(char const *s, char c)
+{
+	int		nb_word;
+	int		i;
+	char	**res;
+
+	if (!s)
+		return (NULL);
+	nb_word = 0;
+	i = 0;
+	while (s[i])
 	{
-		res = malloc(sizeof(char *) * 1 + 1);
-		res[j++] = ft_strdup(str);
-		res[j] = NULL;
-		return (res);
-	}
-	printf("word = %d\n", ft_count_word(str, c, i));
-	res = malloc(sizeof(char *) * ft_count_word(str, c, i) + 1);
-	while (str[i])
-	{
-		while (str[i] == c)
+		while (s[i] && s[i] == c)
 			i++;
-		printf("len %d\n", ft_get_len(str, i, c));
-		res[j] = malloc(sizeof(char) * ft_get_len(str, i, c) + 1);
-		if (str[i] != c && str[i])
-		{
-			while (str[i] != c && str[i])
-			{
-				printf("i = %d j = %d\n", i, j);
-				res[j][x++] = str[i++];
-			}
-			res[j++][x] = '\0';
-		}
-		x = 0;
+		if (s[i])
+			nb_word++;
+		while (s[i] && s[i] != c)
+			i++;
 	}
-	res[j] = NULL;
+	res = malloc(sizeof(char *) * (nb_word + 1));
+	if (!res)
+		return (NULL);
+	res[nb_word] = NULL;
 	return (res);
 }
 
-int	main(int argc, char **argv)
+char	**ft_split(const char *s, char c)
 {
-	char **res;
-	int	i;
+	int		i;
+	int		j;
+	int		k;
+	char	**res;
 
-	if(argc != 3)
-		return (0);
+	res = malloc_res(s, c);
+	if (!res)
+		return (NULL);
+	k = 0;
 	i = 0;
-	printf("str = [%s]\n c = [%c]\n", argv[1], argv[2][0]);
-	res = ft_split(argv[1], 0);
-//	res = ft_split(argv[1], argv[2][0]);
-	while (res[i])
+	while (s[i])
 	{
-		printf("[%d] %s\n", i, res[i]);
-		i++;
+		j = normecmort(&i, s, c);
+		if (j > 0)
+		{
+			res[k++] = ft_substr(s, i, j);
+			if (!res[k - 1])
+				return (error(res, k - 1));
+		}
+		i = i + j;
 	}
-	system("leaks a.out");
+	return (res);
 }
